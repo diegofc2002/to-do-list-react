f877551fcba5986bec5f4f3f4eb9ba3277d5e080
Diego Cardoso <diegofc.estagios@gmail.com>
2026-06-27 17:22:39 -0300

    feat: add task counter and empty state for concluded section

--- src/components/to-do-group/to-do-group.jsx
+++ src/components/to-do-group/to-do-group.jsx
@@ -1,3 +1,4 @@
+import "./to-do-group.style.css";
 import { EmptyState } from "../empty-state/empty-state";
 import { SubHeading } from "../sub-heading/sub-heading";
 import { ToDoItem } from "../to-do-item/to-do-item";
@@ -6,8 +7,13 @@ import { ToDoList } from "../to-do-list/to-do-list";
 export function ToDoGroup({ items, heading }) {
   return (
     <>
-      <SubHeading>{heading}</SubHeading>
+      <SubHeading>
+        {heading} ({items.length})
+      </SubHeading>
       {items.length == 0 && heading == "Para estudar" && <EmptyState />}
+      {items.length == 0 && heading == "Concluído" && (
+        <p className="empty-concluded">Nenhuma tarefa concluída ainda.</p>
+      )}
       <ToDoList>
         {items.map(function (t) {
           return <ToDoItem key={t.id} item={t} />;
